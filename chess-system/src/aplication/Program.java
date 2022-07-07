bb561a61f6a7951d76f8f0d72a359b44a1e54cae
Emerson Oliveira dos Santos <94749757+Emerson143@users.noreply.github.com>
2022-07-06 22:24:17 -0300

    parte 3

--- chess-system/src/aplication/Program.java
+++ chess-system/src/aplication/Program.java
@@ -1,12 +1,13 @@
 package aplication;
 
-import boardgame.Board;
+import Chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board board = new Board (8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
