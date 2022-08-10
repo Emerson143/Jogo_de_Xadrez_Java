b593e7c99fd6b99625fc32be88e7ca69513a38c2
Emerson Oliveira <emerson.143@hotmail.com>
2022-08-10 17:51:15 -0300

    funcuinando

---
+++ chess-system/src/Chess/ChessPiece.java
@@ -0,0 +1,22 @@
+package Chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+
+public abstract class ChessPiece extends Piece {
+
+	private Color color;
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	public Color getColor() {
+		return color;
+	}
+
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
+}
