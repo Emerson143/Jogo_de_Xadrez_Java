7104fd5f4752f0bb6babf6883f15feac5565222a
Emerson Oliveira <emerson.143@hotmail.com>
2022-08-11 21:59:19 -0300

    Capturando as Pesas

--- chess-system/src/Chess/ChessMatch.java
+++ chess-system/src/Chess/ChessMatch.java
@@ -1,16 +1,19 @@
 package Chess;
+import java.util.ArrayList;
+import java.util.List;
+
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
-import Chess.pieces.King;
-import Chess.pieces.Rook;
 public class ChessMatch {
 	
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
-	
-	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
@@ -57,6 +60,12 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if (capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
+		
 		return capturedPiece;
 	}
 	
@@ -87,6 +96,7 @@ public class ChessMatch {
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	
 	private void initialSetup() {
