61fdcbd11018607768a19d0aea9ba995ac454d71
Emerson Oliveira <emerson.143@hotmail.com>
2022-08-22 20:27:18 -0300

    Empassat completo

--- chess-system/src/Chess/ChessMatch.java
+++ chess-system/src/Chess/ChessMatch.java
@@ -130,6 +130,25 @@ public class ChessMatch {
 			board.placePiece(rook, targetT);
 			rook.increaseMoveCount();
 		}
+	
+		//#movimento especial
+		if (p instanceof Pawn) {
+			if (source.getColumn() != target.getColumn() && capturedPiece == null) {
+				Position pawnPosition;
+				if(p.getColor() == Color.WHITE) {
+					pawnPosition = new Position (target.getRow() +1, target.getColumn());
+				}
+				else {
+					pawnPosition = new Position (target.getRow() -1, target.getColumn());
+				}
+				
+				capturedPiece = board.removePiece(pawnPosition);
+				capturedPieces.add(capturedPiece);
+				piecesOnTheBoard.remove(capturedPiece);
+			}
+		}
+	
+	
 		return capturedPiece;
 	}
 
@@ -162,6 +181,24 @@ if (p instanceof King && target.getColumn() == source.getColumn() + 2 ) {
 			board.placePiece(rook, sourceT);
 			rook.increaseMoveCount();
 		}
+	
+	
+	if (p instanceof Pawn) {
+		if (source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
+			ChessPiece pawn = (ChessPiece)board.removePiece(target);
+			Position pawnPosition;
+			if(p.getColor() == Color.WHITE) {
+				pawnPosition = new Position (3, target.getColumn());
+			}
+			else {
+				pawnPosition = new Position (4, target.getColumn());
+			}
+			
+			board.placePiece(pawn, pawnPosition);
+		}
+	}
+
+	
 	}
 	
 	
